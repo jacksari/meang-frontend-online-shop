30a63e80464c5d15b0e8450f0742bd2bd945e2cc
Jack Sari <janasarii@gmail.com>
2020-08-24 22:26:48 -0500

    Estructura base, parte privada y públicasin templates

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: `home`, loadChildren: () =>
+      import('./public/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: `contact`, loadChildren: () =>
+      import('./public/pages/contact/contact.module').then(m => m.ContactModule)
+  },
+  { path: ``, redirectTo: `home`, pathMatch: `full` },
+  { path: `**`, redirectTo: `home`, pathMatch: `full` }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
